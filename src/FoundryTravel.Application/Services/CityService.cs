5b77e907f52601eb7c6d1fa497252e9b1b37ab85
Onur Vatan <onur@techproxy.co.uk>
2026-01-27 20:20:55 +0000

    Implement CityService with repository integration - tests successful

--- src/FoundryTravel.Application/Services/CityService.cs
+++ src/FoundryTravel.Application/Services/CityService.cs
@@ -2,19 +2,38 @@
 using FoundryTravel.Application.Interfaces;
 using FoundryTravel.Domain.Interfaces;
 
-namespace FoundryTravel.Application.Services
-{
-    public class CityService(ICityRepository cityRepository) : ICityService
-    {
-        private readonly ICityRepository _cityRepository = cityRepository;
-        public Task<IEnumerable<CityDto>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+namespace FoundryTravel.Application.Services;
 
-        public Task<CityDto?> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+public class CityService : ICityService
+{
+    private readonly ICityRepository _cityRepository;
+
+    public CityService(ICityRepository cityRepository)
+    {
+        _cityRepository = cityRepository;
+    }
+
+    public async Task<IEnumerable<CityDto>> GetAllAsync()
+    {
+        var cities = await _cityRepository.GetAllAsync();
+
+        return cities.Select(c => new CityDto(
+            c.Id,
+            c.Name,
+            c.Country
+        ));
+    }
+
+    public async Task<CityDto?> GetByIdAsync(Guid id)
+    {
+        var city = await _cityRepository.GetByIdAsync(id);
+        if (city == null)
+            return null;
+
+        return new CityDto(
+            city.Id,
+            city.Name,
+            city.Country
+        );
     }
 }
